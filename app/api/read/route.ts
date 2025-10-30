a92aa0834c5ab77923e3c37b15660e3d33125be0
Henrik uran <henrik.uran.nykmark@nav.no>
2025-10-30 13:16:51 +0100

    oppdatertt router/read

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -6,22 +6,22 @@ export async function GET(request: Request) {
         ? 'https://strim-backend.intern.nav.no/events'
         : 'http://localhost:8080/events';
 
-
+    try {
         let token: string | null;
         if (process.env.NODE_ENV === 'production') {
             token = getToken(request);
             if (!token) {
-                return NextResponse.json({error: 'Missing token'}, {status: 401});
+                return NextResponse.json({ error: 'Missing token' }, { status: 401 });
             }
 
             const validation = await validateToken(token);
             if (!validation.ok) {
-                return NextResponse.json({error: 'Token validation failed'}, {status: 401});
+                return NextResponse.json({ error: 'Token validation failed' }, { status: 401 });
             }
 
             const obo = await requestOboToken(token, 'api://prod-gcp.team-researchops.skup-backend/.default');
             if (!obo.ok) {
-                return NextResponse.json({error: 'OBO token request failed'}, {status: 401});
+                return NextResponse.json({ error: 'OBO token request failed' }, { status: 401 });
             }
 
             token = obo.token;
@@ -33,16 +33,25 @@ export async function GET(request: Request) {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                 'Authorization': `Bearer ${token}`,
+                'Authorization': `Bearer ${token}`,
             },
         });
 
         if (!response.ok) {
             const errorDetails = await response.text();
             console.error('Network response was not ok:', response.status, errorDetails);
-            return NextResponse.json({error: 'Fetch failed', message: "Network response was not ok:"}, {status: 500});
+            throw new Error(`Network response was not ok: ${response.status} - ${errorDetails}`);
         }
 
         const data = await response.json();
         return NextResponse.json(data);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Fetch failed:', error.message, error.stack);
+            return NextResponse.json({ error: 'Fetch failed', message: error.message }, { status: 500 });
+        } else {
+            console.error('An unknown error occurred');
+            return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
+        }
+    }
 }
